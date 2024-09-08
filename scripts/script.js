b08b62bec806b71dcb111477c5dc57f920699909
abdulrahman-58 <160419817+abdulrahman-58@users.noreply.github.com>
2024-09-08 16:20:04 +0500

    this is milestone 1 and 2 made Abdul Rahman Azam

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var toggleSkillsButton = document.getElementById('toggle-skills');
+var skillsSection = document.getElementById('skills');
+toggleSkillsButton === null || toggleSkillsButton === void 0 ? void 0 : toggleSkillsButton.addEventListener('click', function () {
+    if ((skillsSection === null || skillsSection === void 0 ? void 0 : skillsSection.style.display) === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
